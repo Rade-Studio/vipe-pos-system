0a8811d5eb89cdd6c87760ecc9723224637e0f27
Asdrubal Hernandez <ahernand.development@gmail.com>
2025-05-11 16:40:34 -0500

    fix: fixing withoutdraw cash dialog overflow in y column

--- components/cashier/WithdrawCashDialog.tsx
+++ components/cashier/WithdrawCashDialog.tsx
@@ -105,7 +105,7 @@ export function WithdrawCashDialog({ open, onOpenChange, onSuccess }: WithdrawCa
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-[500px]">
+      <DialogContent className="sm:max-w-[500px] max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>Retirar Efectivo</DialogTitle>
         </DialogHeader>
